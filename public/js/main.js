a1c7cfca4cf2226dbd31b5f77aa032fa846a4975
ebenadda <ebenadda@amazon.com>
2022-07-27 18:58:48 -0400

    added a form listener to main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,16 @@
-const chatForm = document.getElementById("chat-Form");
+const chatForm = document.getElementById("chat-form");
 
 const socket = io();
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
 //message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+  console.log(msg);
+});
